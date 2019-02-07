54c3847d0fc4e90e8524f2f8efcddd925383676b
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-02-07 11:25:26 +0100

    change

--- triangle.js
+++ triangle.js
@@ -15,22 +15,23 @@ function myJsFunction() {
 
 
 
-  let a_sum = Math.sum(firstWall, secondWall);
+
+  let a_sum = firstWall + secondWall;
   let b_sum = secondWall + thirdWall;
   let c_sum = thirdWall + firstWall;
   console.log(`${firstWall} ${secondWall} ${thirdWall} ${a_sum} ${b_sum} ${c_sum}`)
 
-  if (firstWall < 0 || secondWall < 0 || thirdWall < 0) {
-    mistake.innerHTML = 'Please enter positive numbers';
-  } else if (thirdWall > a_sum || secondWall > c_sum || firstWall > b_sum) {
+  // if (firstWall < 0 || secondWall < 0 || thirdWall < 0) {
+  //   mistake.innerHTML = 'Please enter positive numbers';
+  // } else if (thirdWall > a_sum || secondWall > c_sum || firstWall > b_sum) {
 
-    mistake.innerHTML = 'The sum of the lengths of any 2 sides of a triangle must be greater than the third side';
-  } else {
-    let a_kampas = (((firstWall ** 2 + secondWall ** 2 - thirdWall ** 2) / (2 * firstWall * secondWall)));
-    let b_kampas = (((secondWall ** 2 + thirdWall ** 2 - firstWall ** 2) / (2 * secondWall * thirdWall)));
-    let c_kampas = (((firstWall ** 2 + thirdWall ** 2 - secondWall ** 2) / (2 * firstWall * thirdWall)));
-    console.log(a_kampas);
-  }
+  //   mistake.innerHTML = 'The sum of the lengths of any 2 sides of a triangle must be greater than the third side';
+  // } else {
+  //   let a_kampas = (((firstWall ** 2 + secondWall ** 2 - thirdWall ** 2) / (2 * firstWall * secondWall)));
+  //   let b_kampas = (((secondWall ** 2 + thirdWall ** 2 - firstWall ** 2) / (2 * secondWall * thirdWall)));
+  //   let c_kampas = (((firstWall ** 2 + thirdWall ** 2 - secondWall ** 2) / (2 * firstWall * thirdWall)));
+  //   console.log(a_kampas);
+  // }
 
 
 
