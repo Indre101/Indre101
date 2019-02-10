69329cd155ad8624c28509c0d193694ca02759b8
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-02-10 19:41:50 +0100

    calculate the degree of triangle

--- triangle.js
+++ triangle.js
@@ -19,21 +19,26 @@ function myJsFunction() {
   let a_sum = firstWall + secondWall;
   let b_sum = secondWall + thirdWall;
   let c_sum = thirdWall + firstWall;
-  console.log(`${firstWall} ${secondWall} ${thirdWall} ${a_sum} ${b_sum} ${c_sum}`)
+  console.log(`${firstWall} ${secondWall} ${thirdWall} ${a_sum} ${b_sum} ${c_sum}`);
 
-  if (firstWall < 0 || secondWall < 0 || thirdWall < 0) {
+  let a_kampas = Math.acos((((firstWall ** 2 + secondWall ** 2 - thirdWall ** 2) / (2 * firstWall * secondWall))));
+  let b_kampas = Math.acos((((secondWall ** 2 + thirdWall ** 2 - firstWall ** 2) / (2 * secondWall * thirdWall))));
+  let c_kampas = Math.acos((((firstWall ** 2 + thirdWall ** 2 - secondWall ** 2) / (2 * firstWall * thirdWall))));
+
+  let degree1 = Math.floor(a_kampas * 180 / Math.PI);
+  let degree2 = Math.floor(b_kampas * 180 / Math.PI);
+  let degree3 = Math.ceil(c_kampas * 180 / Math.PI);
+
+  if (firstWall <= 0 || secondWall <= 0 || thirdWall <= 0) {
     mistake.innerHTML = 'Please enter positive numbers';
-  } else if (thirdWall > a_sum || secondWall > c_sum || firstWall > b_sum) {
+  } else if (thirdWall >= a_sum || secondWall >= c_sum || firstWall >= b_sum) {
 
-    mistake.innerHTML = 'The length of one side can not be longer than the sum of other two';
+    mistake.innerHTML = 'The length of one side can not be equal to or more than the sum of other two';
+  } else if (degree1 === 90 || degree2 === 90 || degree3 === 90) {
+
+    mistake.innerHTML = `The corners are ${degree1}, ${degree2}, ${degree3} the triangle is rectanlge`
   } else {
-    let a_kampas = (((firstWall ** 2 + secondWall ** 2 - thirdWall ** 2) / (2 * firstWall * secondWall)));
-    let b_kampas = (((secondWall ** 2 + thirdWall ** 2 - firstWall ** 2) / (2 * secondWall * thirdWall)));
-    let c_kampas = (((firstWall ** 2 + thirdWall ** 2 - secondWall ** 2) / (2 * firstWall * thirdWall)));
-
-    let degree = Math.floor(a_kampas * 180 / Math.PI);
-    console.log(degree);
-    console.log(a_kampas);
+    mistake.innerHTML = `The corners are ${degree1}, ${degree2}, ${degree3} the triangle is not rectanlge`
   }
 }
 
