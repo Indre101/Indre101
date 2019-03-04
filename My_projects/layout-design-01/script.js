bbaed761d6d63f4d2fb88352295fbe3b4c15b63a
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-03-04 21:59:07 +0100

    triny to make menu items appear one aftr the other

--- My_projects/layout-design-01/script.js
+++ My_projects/layout-design-01/script.js
@@ -2,21 +2,60 @@ let menuelement = document.getElementsByClassName("menuelement");
 let menu = document.getElementById("burger");
 
 
-menuelement.entries()
+let myarray = [];
 
-function displayAll() {
-  console.log(typeof (menuelement))
-  menuelement.forEach((element) => {
-    element.style.display = 'block';
 
-  });
-}
+
 
 
 menu.onclick = function () {
-  displayAll();
-  console.log('object', menuelement);
+
+
+  console.log(myarray);
+  console.log(menuelement);
+
+  setInterval(function () {
+    let a = 8;
+    do {
+
+      for (i in menuelement) {
+        a--
+        menuelement[i].style.display = "block"
+      }
+    } while (a === 7);
+  }, 2000)
+
+
+
+  // for (i in menuelement) {
+  //   menuelement[i].style.display = "block";
+  // }
+
+
+  // setTimeout(function () {
+  //   for (i in menuelement) {
+  //     menuelement[i].style.display = "block";
+  //   }
+  // }, 5)
+
+
+  // for (let i = 0; i < menuelement.length; i++) {
+
+  //   setTimeout(function () {
+  //     menuelement[i].style.display = "block";
+  //   }, 5);
+
+  // }
+
+  //   menuelement[i].style.display = setTimeout(function () {
+  //     menuelement[i].style.display = "block";
+  //   }, 2);
+
+  // }
+  // }
 }
+
+
 
 menu.onmouseover = function () {
   menu.style.backgroundColor = 'red';
