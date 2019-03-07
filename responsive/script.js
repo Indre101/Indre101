e5293e7c8390a22656773e5ded6518f0f7661f5e
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-03-07 19:41:51 +0100

    fixed header for mobile version in index file

--- responsive/script.js
+++ responsive/script.js
@@ -1,15 +1,12 @@
 let menuButton = document.getElementsByClassName("btn-mob-menu");
+let firstMenu = document.getElementById("firstMenu");
+let houseButton = document.getElementById("houseButton");
+let shapeContainer = document.getElementById("shapeContainer");
 
 let myArray = Array.prototype.slice.call(menuButton);
-
-console.log(menuButton)
-
-
 let userHasScrolled = false;
 
-
 window.onscroll = function () {
-  // menuButton.style.opacity = 0.5;
 
   for (i in myArray) {
 
@@ -22,6 +19,36 @@ window.onscroll = function () {
       }, 2000);
     };
   }
+
+
+}
+
+houseButton.onclick = function () {
+
+  firstMenu.style.position = "fixed";
+  // firstMenu.style.top = "530px";
+  firstMenu.style.bottom = "0";
+  shapeContainer.style.height = "700px";
+
+  firstMenu.style.width = "97%";
+  firstMenu.style.opacity = 0.5;
+  firstMenu.style.marginTop = "100px";
+  firstMenu.style.zIndex = -1;
+
+  // firstMenu.style.marginBottom = "-100px";
+
+
+
+
+
+}
+
+firstMenu.onclick = function () {
+
+  firstMenu.style.position = "";
+  firstMenu.style.opacity = 1;
+  shapeContainer.style.height = "";
+
 
 
 }
