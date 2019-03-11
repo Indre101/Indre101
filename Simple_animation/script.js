d097b3b1a73e389303722fce0bcea16df2a95617
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-03-11 22:33:23 +0100

    made all the buttons with color changing function

--- Simple_animation/script.js
+++ Simple_animation/script.js
@@ -1,27 +1,47 @@
 let firstImage = document.getElementById("firstImgae"); //first spongebob image for moving in and out of viewport
-let allButtons = document.getElementsByClassName("movementButton"); /// all the buttons for movement
 const breakBoundries = document.getElementById("boundriesButton"); // movement in and out of viewport
-
-let buttonArray = Array.prototype.slice.call(allButtons); //creating an array from all the buttons
 
 
 //function to make all the buttons change colors of background and font when clicked
+let buttonArray = ["button1", "button2", "button3", "button4", "button5", "button6", "button7", "button8", "button9", "button10", "button11", "button12", "button13", "button14", "button15"]; //creating an array from all the buttons
+let newButtonArray = [];
 
 
-function changeButton(element) {
 
-  buttonArray.forEach(function (element) {
-    element.style.backgroundColor = "white";
-  });
+buttonArray.forEach(function (element) {
+
+  newButtonArray.push(document.getElementById(element));
+});
+
+
+const changeColor = (event) => {
+  event.target.style.backgroundColor = "#006994";
+  event.target.style.transform = "skewX(20deg)";
+}
+
+const regularColor = (event) => {
+
+  event.target.style.backgroundColor = "";
+  event.target.style.transform = "";
 
 
 }
 
-breakBoundries.onclick = function () {
+const changeBackandForth = function (element) {
 
-  firstImage.style.animation = "breakBoundries";
-  firstImage.style.animationIterationCount = "infinite";
-  firstImage.style.animationDuration = "3s";
-  changeButton(element);
 
+  element.onclick = function () {
+    if (element.style.backgroundColor === "") {
+      console.log(element.style.backgroundColor);
+      changeColor(event);
+
+    } else if (element.style.backgroundColor != "") {
+      console.log(element.style.backgroundColor);
+
+      regularColor(event);
+    }
+
+  }
 }
+
+newButtonArray.forEach(changeBackandForth);
