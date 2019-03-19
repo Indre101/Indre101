1b2966f740d9a44e2e3b69cfc0f610d9f24085ec
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-03-19 11:17:49 +0100

    added a second character

--- TheWeekAssignment/script.js
+++ TheWeekAssignment/script.js
@@ -1,10 +1,62 @@
-const start = document.getElementById("btn1");
+const btn1 = document.getElementById("btn1");
 const monster1 = document.getElementById("monster1");
-const styleA = getComputedStyle(start);
+const monter2 = document.getElementById("monster2");
+const Wmonster = document.getElementById("Wmonster");
 
-start.onclick = function () {
 
-  start.style.display = "none";
+
+
+// added animation to the start button
+function firstClick() {
+  btn1.classList.add("changes");
+}
+
+//the start Button
+btn1.addEventListener("animationend", listener, false);
+
+
+function listener(event) {
+  switch (event.type) {
+    case "animationend":
+      btn1.innerHTML = "START?";
+      break;
+
+  }
+}
+
+//the start Button
+
+function isClicked() {
+
+  btn1.style.display = "none";
   monster1.style.display = "block";
 
 }
+
+btn1.onclick = function () {
+  firstClick()
+
+  if (btn1.innerHTML === "START?") {
+    isClicked()
+  }
+}
+
+// finished with the start button
+// animation time
+
+monster1.addEventListener("animationstart", monsterListener, false);
+monster1.addEventListener("animationend", monsterListener, false);
+monster1.addEventListener("animationiteration", monsterListener, false);
+
+
+function monsterListener(event) {
+  switch (event.type) {
+    case "animationend":
+      monter2.style.display = "block";
+      Wmonster.style.display = "block";
+      break;
+
+  }
+}
+
+// animation
