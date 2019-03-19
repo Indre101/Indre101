aa0d11531fc6b5106e4a50ce1c01b2d4d92e3715
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-03-19 08:18:34 +0100

    changed the functionality of utton

--- TheWeekAssignment/script.js
+++ TheWeekAssignment/script.js
@@ -1,9 +1,3 @@
 const start = document.getElementById("btn1");
 
-
-function isClicked() {
-  if (start.style.webkitTransition === "box-shadow 10px 10px 25px 20px hotpink") {
-    start.innerHTML = "Start!";
-  }
-}
-isClicked()
+const styleA = getComputedStyle(start)
