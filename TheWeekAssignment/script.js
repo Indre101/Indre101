037c820e12db5fcdd8a0209c4d5a0c7ea5e2110d
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-03-19 09:52:03 +0100

    added second animation

--- TheWeekAssignment/script.js
+++ TheWeekAssignment/script.js
@@ -1,3 +1,10 @@
 const start = document.getElementById("btn1");
+const monster1 = document.getElementById("monster1");
+const styleA = getComputedStyle(start);
 
-const styleA = getComputedStyle(start)
+start.onclick = function () {
+
+  start.style.display = "none";
+  monster1.style.display = "block";
+
+}
