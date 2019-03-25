23ad0e7dc37a3aadaac0b1845c9baa7fbbf36325
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-03-25 10:38:27 +0100

    exersize with query selector

--- js_excersize/todolist.js
+++ js_excersize/todolist.js
@@ -14,10 +14,35 @@ function crossOut() {
   this.classList.add("crossOut")
   counter++;
 
-  if (counter == 2) {
+  if (counter == 3) {
     this.classList.remove("crossOut");
     counter = 0;
   }
+};
 
+const imageList = document.querySelectorAll("img");
+const backGroundColor = document.querySelector("body");
+
+imageList.forEach(fadeout);
+
+function fadeout(imageElement) {
+
+  imageElement.addEventListener("click", fading)
+
+}
+
+
+
+function fading() {
+  counter++
+  this.classList.toggle("fadeout");
+
+  if (counter == 3) {
+    counter = 0;
+    backGroundColor.style.backgroundColor = "blue";
+  } else if (counter < 3) {
+    backGroundColor.style.backgroundColor = "";
+
+  }
 
 }
