8abd93e371a1a1a2707cd1a86d3c4d780ceba232
indre101 <zygaityte.indre@gmail.com>
2019-03-28 21:19:47 +0100

    created an array with values

--- calculator/script.js
+++ calculator/script.js
@@ -1,5 +1,8 @@
 const input = document.getElementById("input");
 const numbers = document.querySelectorAll(".inputValues");
+const plus = document.getElementById("plus");
+const actions = document.querySelectorAll(".actions");
+
 
 let values = [];
 
@@ -11,5 +14,22 @@ function addValue() {
   values.push(this.value);
   console.log(this.value);
   input.value = values.join("");
+
+}
+// arithmetics
+actions.forEach(doingArithmetics)
+console.log(actions);
+
+function doingArithmetics(element) {
+
+  element.addEventListener("click", arithmetics)
+}
+
+function arithmetics() {
+
+  input.value = "";
+
+
+
 
 }
