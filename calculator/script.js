312c3aba1dc2285f3067ca1c290d584b0a20ce60
indre101 <zygaityte.indre@gmail.com>
2019-04-22 20:47:12 +0200

    styling the calculator

--- calculator/script.js
+++ calculator/script.js
@@ -1,10 +1,12 @@
 const input = document.getElementById("input");
 const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+
 const equality = document.getElementById("equality");
 const numbers = document.querySelectorAll(".inputValues");
-const plus = document.getElementById("plus");
-const actions = document.querySelectorAll(".actions");
+const actions = document.querySelectorAll(".actions")
 const actionclear = document.querySelector(".actionclear");
+
 
 
 
@@ -18,9 +20,63 @@ function clearFields() {
 
 // 
 
-// creating empty array with values
+let theValues = []
 
 let values = [];
+
+
+actions.forEach(function (e) {
+
+  e.addEventListener("click", addAction);
+})
+
+
+function addAction() {
+
+  values = []
+  theValues.push(parseFloat(input1.value));
+
+  let sum = theValues.reduce((acc, val) => {
+    return `${acc} ${this.value} ${val}`;
+  })
+
+
+  input2.value = sum;
+
+  let sum1 = theValues.reduce((acc, val) => {
+
+    switch (this.value) {
+      case "+":
+        return acc + val
+        break;
+      case "-":
+        return acc - val
+        break;
+      case "*":
+        return acc * val
+        break;
+      case "/":
+        return acc / val
+        break;
+      case "=":
+        input1.value = "";
+        input.value = sum1;
+        break;
+
+
+    }
+  })
+
+
+  input.value = sum1;
+
+
+}
+
+
+
+
+// creating empty array with values
 
 numbers.forEach(function (element) {
   element.addEventListener("click", addValue)
@@ -28,28 +84,7 @@ numbers.forEach(function (element) {
 
 function addValue() {
   values.push(this.value);
-  // input1.value = values.join("");
   input1.value = values.join("");
-
-
-
-
-
 }
+
 // 
-
-let theValues = []
-
-
-
-plus.onclick = function () {
-  values = []
-  theValues.push(parseInt(input1.value));
-
-  let sum = theValues.reduce((acc, val) => {
-    return acc + val;
-  })
-
-  input.value = sum;
-
-}
