7f8ab427e0a93af66dc1e04e0d64e072592611f9
indre101 <zygaityte.indre@gmail.com>
2019-04-23 21:48:42 +0200

    drawing squares

--- canvas_work/canvas-resize/script.js
+++ canvas_work/canvas-resize/script.js
@@ -2,3 +2,12 @@ const canvas = document.querySelector("canvas");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// c stands for context
+let c = canvas.getContext("2d");
+
+// c.fillRect(x,y, width, height)
+
+c.fillRect(100, 100, 100, 150);
+c.fillRect(500, 100, 100, 150);
+c.fillRect(300, 400, 100, 150);
