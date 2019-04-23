9159b3c8bfc7c58302828ab551b53a6f131ccf30
indre101 <zygaityte.indre@gmail.com>
2019-04-23 21:54:32 +0200

    changed colors of line and rectangles

--- canvas_work/canvas-resize/script.js
+++ canvas_work/canvas-resize/script.js
@@ -7,7 +7,21 @@ canvas.height = window.innerHeight;
 let c = canvas.getContext("2d");
 
 // c.fillRect(x,y, width, height)
-
+c.fillStyle = "pink";
 c.fillRect(100, 100, 100, 150);
+c.fillStyle = "green"
 c.fillRect(500, 100, 100, 150);
+c.fillStyle = "yellow";
 c.fillRect(300, 400, 100, 150);
+
+// lines
+
+c.beginPath();
+
+// c.moveTo(x,y)
+
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "blue";
+c.stroke();
