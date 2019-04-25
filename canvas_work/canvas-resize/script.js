65f31b7756ab24c2f894f354a376756eadbf7ccc
indre101 <zygaityte.indre@gmail.com>
2019-04-25 18:36:50 +0200

    function moving the circle

--- canvas_work/canvas-resize/script.js
+++ canvas_work/canvas-resize/script.js
@@ -6,34 +6,28 @@ canvas.height = window.innerHeight;
 // c stands for context
 let c = canvas.getContext("2d");
 
-// c.fillRect(x,y, width, height)
-c.fillStyle = "pink";
-c.fillRect(100, 100, 100, 150);
-c.fillStyle = "green"
-c.fillRect(500, 100, 100, 150);
-c.fillStyle = "yellow";
-c.fillRect(300, 400, 100, 150);
+// // c.fillRect(x,y, width, height)
+// c.fillStyle = "pink";
+// c.fillRect(100, 100, 100, 150);
+// c.fillStyle = "green"
+// c.fillRect(500, 100, 100, 150);
+// c.fillStyle = "yellow";
+// c.fillRect(300, 400, 100, 150);
 
-// lines
+// // lines
 
-c.beginPath();
+// c.beginPath();
 
-// c.moveTo(x,y)
+// // c.moveTo(x,y)
 
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "blue";
-c.stroke();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "blue";
+// c.stroke();
 
 //arc
 
-
-// c.arc(x:int, y:int, r:int, startAngle: Float32Array, endAngel:Float32Array, drawCountterClockwise:Boolean(false))
-c.beginPath();
-c.arc(300, 300, 30, 0, Math.PI * 2, false);
-c.strokeStyle = "black";
-c.stroke()
 
 // creating 100 circles
 
@@ -49,15 +43,59 @@ function randomColors() {
 }
 
 
-for (let i = 0; i < 100; i++) {
+// for (let i = 0; i < 100; i++) {
 
 
 
-  let x = Math.random() * window.innerWidth;
-  let y = Math.random() * window.innerHeight;
+//   let x = Math.random() * window.innerWidth;
+//   let y = Math.random() * window.innerHeight;
+//   c.beginPath();
+
+//   c.arc(x, y, 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = randomColors();
+//   c.stroke()
+// }
+
+// c.arc(x:int, y:int, r:int, startAngle: Float32Array, endAngel:Float32Array, drawCountterClockwise:Boolean(false))
+
+
+
+
+
+// for (let i = 0; i < 100; i++) {
+//   c.beginPath();
+//   x += dx;
+//   y += dy;
+//   c.arc(x, y, 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = "black";
+//   c.stroke()
+// }
+
+
+
+let x = Math.random() * window.innerWidth;
+let dx = (Math.random() - 0.5) + 5;
+let y = Math.random() * window.innerHeight;
+let dy = (Math.random() - 0.5) + 5;
+let radiusC = 30;
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
   c.beginPath();
-
-  c.arc(x, y, 30, 0, Math.PI * 2, false);
-  c.strokeStyle = randomColors();
+  x += dx;
+  y += dy;
+  c.arc(x, y, radiusC, 0, Math.PI * 2, false);
   c.stroke()
+  if (x + radiusC > window.innerWidth || x - radiusC < 0) {
+
+    c.strokeStyle = randomColors();
+    dx = -dx;
+  } else if (y + radiusC > window.innerHeight || y - radiusC < 0) {
+    dy = -dy;
+    c.strokeStyle = randomColors();
+
+  }
 }
+
+animate();
