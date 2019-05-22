710da5b90341723e29ca1a4d4b33a3cefb864059
indre101 <zygaityte.indre@gmail.com>
2019-05-22 10:30:39 +0200

    started a form

--- calculator/script.js
+++ calculator/script.js
@@ -2,6 +2,8 @@ const input1 = document.getElementById("input1");
 const input = document.getElementById("input");
 const valueNumbers = document.querySelectorAll(".inputValues");
 const actionsMath = document.querySelectorAll(".act")
+
+
 let valueArray = [];
 
 
@@ -13,8 +15,13 @@ valueNumbers.forEach((e) => {
 
 // displays numbers 
 
-function pushValue() {
+function clearTheField() {
+  valueArray = []
+  input1.textContent = "";
 
+}
+
+function pushValue() {
   valueArray.push(parseFloat(this.innerHTML));
   console.log(valueArray);
   input1.innerHTML = valueArray.join("");
@@ -33,35 +40,94 @@ actionsMath.forEach((f) => {
 
 
 let caclculationValues = [];
+let finalValueArray = [];
+
+
 
 function actionsOf() {
-  valueArray = [];
+
   caclculationValues.push(parseFloat(input1.innerHTML))
-  console.log("hjjk")
-  input.innerHTML
-  let a;
-  caclculationValues.reduce((accumulator, currentValue) => {
+
+  let lastValue = finalValueArray[finalValueArray.length - 1]
+  let lastValueOfSubmitted = caclculationValues[caclculationValues.length - 1]
+  // let previousValue = finalValueArray[finalValueArray.length - 2]
+
+  finalValueArray.push(parseFloat(input.innerHTML))
+  console.log(finalValueArray);
+
+  let bValue;
+
+
+
+  if (finalValueArray.length < 3) {
+    input.innerHTML = caclculationValues.reduce((accumulator, decantor) => {
+      switch (this.innerHTML) {
+
+        case "+":
+
+
+          return accumulator + decantor
+          break;
+        case "-":
+          return accumulator - decantor
+
+          break;
+        case "*":
+          return accumulator * decantor
+
+          break;
+        case "/":
+          return accumulator / decantor
+
+          break;
+      }
+    })
+
+  } else if (finalValueArray.length >= 3) {
+
+
     switch (this.innerHTML) {
       case "+":
-        a = accumulator + currentValue;
+        console.log("antras if");
+
+        bValue = lastValue + lastValueOfSubmitted;
+        console.log(bValue + "bValue")
+        console.log(lastValue + "lastValue")
+        console.log(lastValueOfSubmitted + "lastValueOfSubmitted")
+        return bValue;
         break;
       case "-":
-        a = accumulator - currentValue;
+        bValue = lastValue - lastValueOfSubmitted;
+        console.log(bValue + "bValue")
+        console.log(lastValue + "lastValue")
+        console.log(lastValueOfSubmitted + "lastValueOfSubmitted")
+        return bValue;
+
         break;
       case "*":
-        a = accumulator * currentValue;
+        bValue = lastValue * lastValueOfSubmitted;
+        console.log(bValue + "bValue")
+        console.log(lastValue + "lastValue")
+        console.log(lastValueOfSubmitted + "lastValueOfSubmitted")
+        return bValue;
         break;
       case "/":
-        a = accumulator - currentValue;
+        bValue = lastValue / lastValueOfSubmitted;
+        console.log(bValue + "bValue")
+        console.log(lastValue + "lastValue")
+        console.log(lastValueOfSubmitted + "lastValueOfSubmitted")
+        return bValue;
         break;
 
     }
-    console.log(a);
 
-  })
+    input.innerHTML = bValue;
 
+
+  }
 
 }
+
 
 
 
