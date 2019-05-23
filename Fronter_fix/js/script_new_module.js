eadb23d00b3609408c470b973d5f293a17349447
indre101 <zygaityte.indre@gmail.com>
2019-05-23 20:41:05 +0200

    fixed the current project filter

--- Fronter_fix/js/script_new_module.js
+++ Fronter_fix/js/script_new_module.js
@@ -78,26 +78,22 @@ function filterByTheme() {
 
 
   if (colorBG == "rgba(255, 180, 170, 0.5)") {
-    console.log("themesmm")
     document.getElementById("theme2").style.display = "none";
     document.getElementById("theme3").style.display = "none";
     document.getElementById("theme4").style.display = "none";
 
   } else if (colorBG == "rgba(110, 206, 178, 0.3)") {
-    console.log("themesmm")
     document.getElementById("theme2").style.display = "block";
     document.getElementById("theme3").style.display = "none";
     document.getElementById("theme4").style.display = "none";
 
   } else if (colorBG == "rgba(255, 103, 31, 0.3)") {
-    console.log("themesmm")
     document.getElementById("theme3").style.display = "block";
 
     document.getElementById("theme2").style.display = "none";
     document.getElementById("theme4").style.display = "none";
 
   } else if (colorBG == "rgba(240, 78, 152, 0.3)") {
-    console.log("themesmm")
     document.getElementById("theme2").style.display = "none";
     document.getElementById("theme3").style.display = "none";
     document.getElementById("theme4").style.display = "block";
@@ -111,14 +107,83 @@ const datesOf = document.querySelectorAll(".date");
 
 datesOf.forEach((j) => {
 
-  let year = [2018, 2019];
 
-  let a = Math.floor(Math.random() * 2);
-  let b = Math.floor((Math.random() * 12) + 1);
+  let b = Math.floor((Math.random() * 5) + 1);
   let c = Math.floor((Math.random() * 28) + 1);
 
-  let d = new Date(`${b} ${c}, ${year[a]}`)
-  console.log(d);
-  j.innerHTML = d;
+  let d = new Date(`${b}- ${c}- 2019`)
+  j.innerHTML = d.toLocaleDateString("en-US");
 
 })
+
+// filtering by date
+
+const btn3 = document.getElementById("btn3");
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
+
+
+let today1 = today.toLocaleDateString("en-US")
+
+
+
+var de = new Date();
+de.setDate(de.getDate() - 20);
+
+de2 = de.toLocaleDateString("en-US")
+
+
+// newwest project
+
+const filterResult = document.getElementById("filterResult");
+
+btn3.onclick = function () {
+
+  for (let h = 0; h < datesOf.length; h++) {
+
+    let dateToCompare = new Date(datesOf[h].innerHTML);
+    console.log(dateToCompare);
+
+    if (dateToCompare <= today && dateToCompare > de) {
+      console.log("blk")
+      filterResult.innerHTML = dateToCompare;
+    }
+
+
+  }
+
+
+}
+
+
+
+let firstDate = document.getElementById("dateInput1");
+let second = document.getElementById("dateInput2");
+let filterDatebutton = document.getElementById("filterDatebutton");
+
+
+filterDatebutton.onclick = function () {
+  event.preventDefault();
+  let dof = new Date(datesOf[0].innerHTML);
+  console.log(past1)
+  console.log(today1);
+
+
+  if (+dof >= +today) {
+    console.log("pirmas");
+  } else if (+dof <= +today) {
+    console.log("antras");
+
+
+  }
+
+  // else if (07 / 07 / 2020 > today) {
+  //   console.log("antras");
+  //   console.log(datesOf[0].innerHTML)
+  //   console.log(today);
+  // }
+
+}
