49fce0dab0bc2fa3518007817b8d27c8efebd81b
indre101 <zygaityte.indre@gmail.com>
2019-05-25 21:26:02 +0200

    added a search function by name

--- Fronter_fix/js/contacts.js
+++ Fronter_fix/js/contacts.js
@@ -1,11 +1,51 @@
 const searchButton = document.getElementById("searchButton");
-
+const searchInput = document.getElementById("insertName");
 const nameOfKea = document.querySelectorAll(".nameOfKea");
 const emailOfKea = document.querySelectorAll(".emailOfKea");
 const keaListContainer = document.getElementById("keaListContainer");
 
+
+
+let resultName = []
+let resultNameID = [];
+
+
 searchButton.onclick = function () {
   event.preventDefault();
+  searchTheName();
+}
+
+function searchTheName() {
+
+  nameOfKea.forEach((n) => {
+
+
+    if (n.innerHTML.toLowerCase().includes(searchInput.value.toLowerCase())) {
+      resultName.push(n.innerHTML)
+      console.log(n.id);
+      let x_1 = document.getElementById(n.id).parentElement
+      appednResult(x_1)
+    }
+  })
+}
+
+
+
+function checkIfNameBelongs() {
+
+  // for()
+
+}
+
+
+// appending function
+
+function appednResult(res) {
+
+  res.style.backgroundColor = "rgba(253,215,87,0.3)"
+
+  keaListContainer.insertAdjacentElement('afterbegin', res)
+
 
 
 }
