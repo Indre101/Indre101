ce492ef4f7bee03f179e0fe8e3755fb4ea98c666
indre101 <zygaityte.indre@gmail.com>
2019-05-25 21:59:21 +0200

    changed search result display

--- Fronter_fix/js/contacts.js
+++ Fronter_fix/js/contacts.js
@@ -19,11 +19,14 @@ function searchTheName() {
 
   nameOfKea.forEach((n) => {
 
+    document.getElementById(n.id).parentElement.classList.remove("resultName");
 
     if (n.innerHTML.toLowerCase().includes(searchInput.value.toLowerCase())) {
-      resultName.push(n.innerHTML)
+      // resultName.push(n.innerHTML)
+
       console.log(n.id);
       let x_1 = document.getElementById(n.id).parentElement
+
       appednResult(x_1)
     }
   })
@@ -38,11 +41,14 @@ function checkIfNameBelongs() {
 }
 
 
+
 // appending function
 
 function appednResult(res) {
 
-  res.style.backgroundColor = "rgba(253,215,87,0.3)"
+
+  // res.style.backgroundColor = "rgba(253,215,87,0.3)"
+  res.classList.add("resultName");
 
   keaListContainer.insertAdjacentElement('afterbegin', res)
 
