dee2cdd725ff18260304ce1b756b90168731c131
indre101 <zygaityte.indre@gmail.com>
2019-05-27 15:00:53 +0200

    added feedback

--- Fronter_fix/js/submissions.js
+++ Fronter_fix/js/submissions.js
@@ -1,24 +1,21 @@
 let submissionsList = document.getElementById("submissionsList");
-
+const filterButton_2 = document.getElementById("filterButton_2");
 
 
 let status1 = new Image();
 status1.src = "../icons/anX.png"
-status1.classList.add("statusImage")
 
 let status2 = new Image();
 status2.src = "../icons/Check.png"
-status2.classList.add("statusImage")
 
 let status3 = new Image();
 status3.src = "../icons/threeDots.png";
-status3.classList.add("statusImage")
 
+let statusArray = [status1.src, status2.src, status3.src];
 
 function setStatus() {
   let a_1 = Math.floor(Math.random() * 3);
-  let statusArray = [status1, status2, status3];
-  return statusArray[a_1];
+  return a_1
 }
 
 function setDate() {
@@ -41,51 +38,82 @@ function setName() {
 }
 
 
-// let Submission = {
-//   name: function () {
-//     this.name = subNameArray[e];
-//     return this.name
 
-//   },
-//   date: d1,
-//   status: statusArray[a_1]
-
-// }
-
-// function name(params) {
-
-// }
-
-
-
-// var animal1 = Object.create(Submission);
 
 for (let index = 0; index < 12; index++) {
-
 
   let newDiv = document.createElement("div");
   let newH = document.createElement("h4");
   let newH_2 = document.createElement("h4");
+  let newImgDiv = document.createElement("div");
+
+
+  newImgDiv.style.backgroundImage = "url('" + statusArray[setStatus()] + "')";
+
+  newImgDiv.classList.add("statusImage")
 
   newH_2.textContent = setName();
   newH.textContent = setDate();
+  newH.classList.add("dateClass");
 
 
+  newDiv.appendChild(newImgDiv);
   newDiv.appendChild(newH_2)
-  newDiv.appendChild(setStatus())
   newDiv.appendChild(newH)
 
 
   newDiv.classList.add("theDiv");
 
+  newDiv.onclick = function () {
+    addComment(newDiv, newImgDiv)
+  }
 
-  appendChild(newDiv);
-
+  submissionsList.appendChild(newDiv);
 
 }
 
-function appendChild(element) {
-  submissionsList.appendChild(element);
+
+
+function addComment(divParent, imageStatus) {
+
+  let commentDiv = document.createElement("div");
+  let commentH = document.createElement("h4");
+  commentH.textContent = "Great work, Kind Regards";
+  commentDiv.appendChild(commentH);
+  imageStatus.style.display = "none";
+  commentDiv.style.display = "block";
+
+  divParent.insertAdjacentElement("afterbegin", commentDiv);
+
+  divParent.onclick = function () {
+
+    commentDiv.style.display = "none";
+    imageStatus.style.display = "block";
+
+
+  }
+
+}
+
+
+const dateClass = document.querySelectorAll(".dateClass");
+
+let dateClass_1 = [];
+
+
+filterButton_2.onclick = function name() {
+
+  for (let index = 0; index < dateClass.length; index++) {
+
+
+    dateClass_1.push(dateClass[index].innerHTML);
+    let dateClass_2 = dateClass_1.sort();
+
+    console.log(dateClass_2)
+
+
+
+  }
 
 
 }
