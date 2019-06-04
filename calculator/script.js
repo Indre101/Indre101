ec35fd188dd46829dc477267323872218fb6c830
indre101 <zygaityte.indre@gmail.com>
2019-06-04 19:31:28 +0200

    changed the functions

--- calculator/script.js
+++ calculator/script.js
@@ -1,134 +1,26 @@
-const input1 = document.getElementById("input1");
-const input = document.getElementById("input");
-const valueNumbers = document.querySelectorAll(".inputValues");
-const actionsMath = document.querySelectorAll(".act")
+const result = document.getElementById("result")
+const values = document.querySelectorAll(".values");
 
-
-let valueArray = [];
-
-
-valueNumbers.forEach((e) => {
-
-  e.addEventListener("click", pushValue)
-
-})
-
-// displays numbers 
-
-function clearTheField() {
-  valueArray = []
-  input1.textContent = "";
-
-}
-
-function pushValue() {
-  valueArray.push(parseFloat(this.innerHTML));
-  console.log(valueArray);
-  input1.innerHTML = valueArray.join("");
-}
-
-
-// adding another numbers
-
-
-actionsMath.forEach((f) => {
-
-  f.addEventListener("click", actionsOf)
-
-
+values.forEach((e) => {
+  e.addEventListener("click", dis)
 })
 
 
-let caclculationValues = [];
-let finalValueArray = [];
 
+function dis() {
+  result.value += this.value
+}
 
+//function that evaluates the digit and return result 
+function solve() {
 
-function actionsOf() {
-
-  caclculationValues.push(parseFloat(input1.innerHTML))
-
-  let lastValue = finalValueArray[finalValueArray.length - 1]
-  let lastValueOfSubmitted = caclculationValues[caclculationValues.length - 1]
-  // let previousValue = finalValueArray[finalValueArray.length - 2]
-
-  finalValueArray.push(parseFloat(input.innerHTML))
-  console.log(finalValueArray);
-
-  let bValue;
-
-
-
-  if (finalValueArray.length < 3) {
-    input.innerHTML = caclculationValues.reduce((accumulator, decantor) => {
-      switch (this.innerHTML) {
-
-        case "+":
-
-
-          return accumulator + decantor
-          break;
-        case "-":
-          return accumulator - decantor
-
-          break;
-        case "*":
-          return accumulator * decantor
-
-          break;
-        case "/":
-          return accumulator / decantor
-
-          break;
-      }
-    })
-
-  } else if (finalValueArray.length >= 3) {
-
-
-    switch (this.innerHTML) {
-      case "+":
-        console.log("antras if");
-
-        bValue = lastValue + lastValueOfSubmitted;
-        console.log(bValue + "bValue")
-        console.log(lastValue + "lastValue")
-        console.log(lastValueOfSubmitted + "lastValueOfSubmitted")
-        return bValue;
-        break;
-      case "-":
-        bValue = lastValue - lastValueOfSubmitted;
-        console.log(bValue + "bValue")
-        console.log(lastValue + "lastValue")
-        console.log(lastValueOfSubmitted + "lastValueOfSubmitted")
-        return bValue;
-
-        break;
-      case "*":
-        bValue = lastValue * lastValueOfSubmitted;
-        console.log(bValue + "bValue")
-        console.log(lastValue + "lastValue")
-        console.log(lastValueOfSubmitted + "lastValueOfSubmitted")
-        return bValue;
-        break;
-      case "/":
-        bValue = lastValue / lastValueOfSubmitted;
-        console.log(bValue + "bValue")
-        console.log(lastValue + "lastValue")
-        console.log(lastValueOfSubmitted + "lastValueOfSubmitted")
-        return bValue;
-        break;
-
-    }
-
-    input.innerHTML = bValue;
-
-
-  }
+  let x = result.value
+  let y = eval(x)
+  result.value = y
 
 }
 
-
-
-
-// actions
+//function that clear the display 
+function clr() {
+  result.value = ""
+}
