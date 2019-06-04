0f63ed9c7431f62847ac5ffb82a78f24d22df3ab
indre101 <zygaityte.indre@gmail.com>
2019-06-04 18:16:40 +0200

    added image

--- canvas_02/js/script.js
+++ canvas_02/js/script.js
@@ -1,6 +1,35 @@
 let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+canvas.width = canvas.scrollWidth;
+canvas.height = canvas.scrollHeight;
+// let ctx = canvas.getContext("2d");
+// context.drawImage(image, 0, 0, );
 
-ctx.clearRect()
+// ctx.clearRect();
+window.onload = function() {
+  let ctx = canvas.getContext("2d");
 
-ctx.fillRect(100, 100, 100, 100);
+  let img_1 = new Image();
+  // img_1.style.objectFit = "contain";
+
+  img_1.onload = function() {
+    let tempCanvas = document.createElement("canvas");
+    let tempCtx = tempCanvas.getContext("2d");
+    // tempCanvas.classList.add("canvas");
+
+    tempCanvas.width = 600;
+    tempCanvas.height = 500;
+
+    tempCtx.drawImage(img_1, 0, 0, tempCanvas.width, tempCanvas.height);
+    // ctx.drawImage(img_1, 0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < 20; i++) {
+      for (let j = 0; j < 20; j++) {
+        let x = i * 40;
+        let y = j * 40;
+
+        ctx.drawImage(tempCanvas, x, y, 40, 40, x + i * 5, y, 40, 40);
+      }
+    }
+  };
+  img_1.src = "./img/naujas.jpg";
+};
