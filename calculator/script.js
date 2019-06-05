0f6fecee84f25ec0d50516d3f358c23b6b94e0b7
indre101 <zygaityte.indre@gmail.com>
2019-06-05 19:20:01 +0200

    started drawing

--- calculator/script.js
+++ calculator/script.js
@@ -24,3 +24,47 @@ function solve() {
 function clr() {
   result.value = ""
 }
+
+// canvas
+
+const canvas1 = document.getElementById("canvas_1");
+const ctx = canvas1.getContext("2d");
+
+
+canvas1.width = canvas1.scrollWidth
+canvas1.height = canvas1.scrollHeight
+
+
+
+// ctx.beginPath();
+
+// ctx.moveTo(20, 30);
+// ctx.lineTo(200, 200);
+// ctx.stroke();
+// canvas1.width = window.innerWidth
+// canvas1.height = window.innerHeight
+
+
+// function draw() {
+
+ctx.beginPath();
+
+ctx.moveTo(window.innerWidth, 30);
+ctx.lineTo(200, 200);
+ctx.lineTo(200, window.innerHeight);
+ctx.stroke();
+
+ctx.beginPath()
+ctx.moveTo(200, 200);
+ctx.lineTo(900, 200);
+
+
+
+ctx.stroke();
+
+
+// }
+
+
+
+// window.addEventListener("resize", draw);
