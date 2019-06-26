f0a1905ad2a412b109a0b408c09504f1e8174fab
indre101 <zygaityte.indre@gmail.com>
2019-06-26 14:26:58 +0200

    cleaned the repo

--- Portfolio/canvas.js
+++
@@ -1,9 +0,0 @@
-const canvas_1 = document.getElementById("canvas_1");
-const ctx2 = canvas_1.getContext("2d");
-
-canvas_1.width = canvas_1.scrollWidth;
-canvas_1.height = canvas_1.scrollHeight;
-
-
-ctx2.rect(10, 10, 10, 10)
-ctx2.stroke()
