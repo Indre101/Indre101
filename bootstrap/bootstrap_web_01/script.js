feda5d05fefd7a5a66402d2d2b15af59ebdf670d
indre101 <zygaityte.indre@gmail.com>
2019-06-30 21:01:10 +0200

    fixed the menu

--- bootstrap/bootstrap_web_01/script.js
+++ bootstrap/bootstrap_web_01/script.js
@@ -1,11 +1,12 @@
 const menuButtonIcon = document.getElementById("menuButtonIcon");
-
+const landingPagePart = document.getElementById("landingPagePart");
 
 let menuClick = 0;
 
 menuButtonIcon.onclick = function () {
 
   menuClick++
+  landingPagePart.style.backgroundImage = "linear-gradient(rgba(40, 60, 200, 0.3), rgba(40, 60, 200, 0.3)), url(./img/landing-page-bg.jpg)"
 
   menuButtonIcon.style.backgroundImage = "none";
   menuButtonIcon.innerHTML = "&times;"
@@ -14,10 +15,13 @@ menuButtonIcon.onclick = function () {
     menuClick = 0;
     menuButtonIcon.style.backgroundImage = "";
     menuButtonIcon.innerHTML = ""
+    landingPagePart.style.backgroundImage = ""
+
+
+
   }
 
 
 }
-
 // content: "\f00d";
 // content: "\f0c9";
