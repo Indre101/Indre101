bac161b0d9eab4a7bcaafddc62a823f4b9995027
indre101 <zygaityte.indre@gmail.com>
2019-07-01 13:12:24 +0200

    fixed the  menu button

--- bootstrap/bootstrap_web_01/script.js
+++ bootstrap/bootstrap_web_01/script.js
@@ -1,25 +1,28 @@
 const menuButtonIcon = document.getElementById("menuButtonIcon");
-const landingPagePart = document.getElementById("landingPageText");
-const navbarSupportedContent = document.getElementById("navbarSupportedContent");
+const landingPagePart = document.getElementById("page1");
+const navbarSupportedContent = document.getElementById("navbarToggler01");
 
 let menuClick = 0;
 
 menuButtonIcon.onclick = function () {
 
   menuClick++
-  landingPagePart.classList.add("after");
 
+  landingPagePart.classList.add("after");
   menuButtonIcon.style.backgroundImage = "none";
   menuButtonIcon.innerHTML = "&times;"
+  navbarSupportedContent.style.display = "block";
+
   navbarSupportedContent.classList.remove("dissapeartoLeft");
 
 
   if (menuClick === 2) {
     menuClick = 0;
     menuButtonIcon.style.backgroundImage = "";
-    menuButtonIcon.innerHTML = ""
-    landingPagePart.classList.remove("after")
     navbarSupportedContent.classList.add("dissapeartoLeft");
+    menuButtonIcon.innerHTML = ""
+
+    landingPagePart.classList.remove("after")
   }
 
 
