cb1266bba31b07708a53e1764d012ae909f9a4cf
indre101 <zygaityte.indre@gmail.com>
2019-07-08 22:54:00 +0200

    changed onclick event

--- bootstrap/boostrap_web_02/js/script.js
+++ bootstrap/boostrap_web_02/js/script.js
@@ -2,23 +2,12 @@
 
 const menuExpandButton = document.querySelectorAll(".buttonExpand");
 
-
-menuExpandButton.forEach((e) => {
-
-  e.onclick = function () {
-
-
-
-    menuExpandButton.forEach((m) => {
+menuExpandButton.forEach(e => {
+  e.onclick = function() {
+    menuExpandButton.forEach(m => {
       m.classList.remove("menu-expand-button-clicked");
-
-    })
+    });
 
     this.classList.add("menu-expand-button-clicked");
-
-
-  }
-
-
-
-})
+  };
+});
