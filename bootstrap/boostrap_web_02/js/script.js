a9ba258e0cb5d62bf020a3ec768c407766ecd588
indre101 <zygaityte.indre@gmail.com>
2019-07-08 23:27:05 +0200

    added if statement

--- bootstrap/boostrap_web_02/js/script.js
+++ bootstrap/boostrap_web_02/js/script.js
@@ -6,18 +6,29 @@ let clickCount = 0;
 
 menuExpandButton.forEach(e => {
   e.onclick = function() {
+    let element = window.getComputedStyle(this);
+
     menuExpandButton.forEach(m => {
       m.classList.remove("menu-expand-button-clicked");
     });
+    this.classList.add("menu-expand-button-clicked");
 
-    if (clickCount === 0) {
-      this.onclick = function() {
-        clickCount++;
-        this.classList.add("menu-expand-button-clicked");
-      };
-    } else if (clickCount === 1) {
-      clickCount = 0;
+    if (element.style.getPropertyValue("backgroundColor") != "") {
+      console.log("hjk");
       this.classList.remove("menu-expand-button-clicked");
     }
+
+    // if (clickCount === 0) {
+    //   this.onclick = function() {
+    //     clickCount++;
+    //     this.classList.add("menu-expand-button-clicked");
+    //   };
+    // } else if (clickCount === 1) {
+    //   this.onclick = function() {
+    //     clickCount = 0;
+
+    //     this.classList.remove("menu-expand-button-clicked");
+    //   };
+    // }
   };
 });
