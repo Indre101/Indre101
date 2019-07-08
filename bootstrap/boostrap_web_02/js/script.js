8d9698fc3da98e3e81d397fb3afc6da6fb024117
indre101 <zygaityte.indre@gmail.com>
2019-07-08 23:02:53 +0200

    added if statement

--- bootstrap/boostrap_web_02/js/script.js
+++ bootstrap/boostrap_web_02/js/script.js
@@ -2,18 +2,22 @@
 
 const menuExpandButton = document.querySelectorAll(".buttonExpand");
 
+let clickCount = 0;
+
 menuExpandButton.forEach(e => {
   e.onclick = function() {
     menuExpandButton.forEach(m => {
       m.classList.remove("menu-expand-button-clicked");
     });
 
-    this.classList.add("menu-expand-button-clicked");
-
-    if (this.getElementsByClassName.backgroundColor != "") {
+    if (clickCount === 0) {
       this.onclick = function() {
-        this.classList.remove("menu-expand-button-clicked");
+        clickCount++;
+        this.classList.add("menu-expand-button-clicked");
       };
+    } else if (clickCount === 1) {
+      clickCount = 0;
+      this.classList.remove("menu-expand-button-clicked");
     }
   };
 });
