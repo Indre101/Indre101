88c8eceed93da612175ffb831e2bc70f75c479e7
indre101 <zygaityte.indre@gmail.com>
2019-07-08 23:42:04 +0200

    added get computed style

--- bootstrap/boostrap_web_02/js/script.js
+++ bootstrap/boostrap_web_02/js/script.js
@@ -6,29 +6,24 @@ let clickCount = 0;
 
 menuExpandButton.forEach(e => {
   e.onclick = function() {
-    let element = window.getComputedStyle(this);
-
     menuExpandButton.forEach(m => {
       m.classList.remove("menu-expand-button-clicked");
     });
-    this.classList.add("menu-expand-button-clicked");
 
-    if (element.style.getPropertyValue("backgroundColor") != "") {
+    let colorBG = window
+      .getComputedStyle(this, null)
+      .getPropertyValue("background-color");
+
+    if (colorBG === "#c736c2") {
       console.log("hjk");
-      this.classList.remove("menu-expand-button-clicked");
+
+      this.onclick = function() {
+        this.classList.remove("menu-expand-button-clicked");
+      };
+    } else {
+      this.onclick = function() {
+        this.classList.add("menu-expand-button-clicked");
+      };
     }
-
-    // if (clickCount === 0) {
-    //   this.onclick = function() {
-    //     clickCount++;
-    //     this.classList.add("menu-expand-button-clicked");
-    //   };
-    // } else if (clickCount === 1) {
-    //   this.onclick = function() {
-    //     clickCount = 0;
-
-    //     this.classList.remove("menu-expand-button-clicked");
-    //   };
-    // }
   };
 });
