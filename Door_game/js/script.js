9359e7341ecef76e955e5c5ec55c2911688fd2f8
indre101 <zygaityte.indre@gmail.com>
2019-07-29 21:09:31 +0200

    found a bug

---
+++ Door_game/js/script.js
@@ -0,0 +1,18 @@
+const landingPageContainer = document.getElementById("landingPageContainer");
+const startGamePage = document.getElementById("startGamePage");
+const
+
+  console.log("hjæk")
+
+// chaning background image 
+// url('../images/images/bg_horror_v_2.jpg')
+function changeBackground(x, u) {
+
+  u = "";
+  x.style.backgroundImage = u;
+
+}
+
+
+
+changeBackground(startGamePage, "url('./images/images/bg_2.jpg')");
