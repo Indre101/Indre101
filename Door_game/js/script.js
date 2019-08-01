59c6328a7b9d858b5e0f9787e453c7618df3110c
indre101 <zygaityte.indre@gmail.com>
2019-08-01 11:32:11 +0200

    created function to randomise the behid doors options

--- Door_game/js/script.js
+++ Door_game/js/script.js
@@ -73,16 +73,64 @@ const addDoorTest = document.getElementById("addDoorTest");
 
 // declared variables for function to add another column once level up
 const htmlColumn = window.getComputedStyle(document.querySelector("html"));
+const doorContainer = document.getElementById("styleOftheDoorContainer");
 const colNum = parseInt(htmlColumn.getPropertyValue("--colNum"));
 let columnNumber = colNum
+
+let gameDoor = document.querySelectorAll(".entrance")
+
+
+
+
+let testWords = ["khg", "sad", "ojkæ", "kjhm", "jæk", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+
+
 
 
 
 // function to add another grid column
 addDoorTest.onclick = function () {
 
+
+
+
   columnNumber++
-  console.log("gkhl")
   document.documentElement.style.setProperty("--colNum", columnNumber);
+
+
+  let elmnt = document.querySelector(".entrance");
+  let cln = elmnt.cloneNode(true);
+  doorContainer.appendChild(cln);
+
+
+  // let newDoor = document.createElement("<div class=" + "entrance" + "><img  class=" + "doors  frame-w " + " src=" + "./images/images/door-frame_v_3.png" + " alt=" + "><img  class=" + "doors door-w door-animation" + " src=" + "./images/images/door_2_1.png" + "><h2 class=" + "testWord" + "></h2></div>");
+
+  // doorContainer.appendChild(newDoor);
+
+  let testWordsElements = document.querySelectorAll(".testWord")
+
+
+  testWordsElements.forEach((e) => {
+    let randomNumber = Math.floor(Math.random() * testWords.length);
+
+
+    e.innerHTML = testWords[randomNumber];
+  })
+
+
+  // gameDoor.forEach((e) => {
+  //   let randomNumber = Math.floor(Math.random() * testWords.length);
+
+
+  //   let testWord = document.createElement("p");
+
+
+  //   testWord.textContent = testWords[randomNumber];
+  //   cln.appendChild(testWord);
+  //   e.appendChild(testWord);
+  // })
+
 
 }
