aebab89c913b3d4ae50ca9eb5b0869bc30e035c3
indre101 <zygaityte.indre@gmail.com>
2019-08-20 19:46:47 +0200

    added different strokes

--- paint/script.js
+++ paint/script.js
@@ -11,37 +11,6 @@ const ctx = canvas.getContext("2d");
 
 
 
-
-
-
-
-
-
-tools.forEach(t => {
-
-  t.onclick = function () {
-
-    tools.forEach(t => {
-
-      t.style.backgroundColor = "blue";
-
-    })
-
-
-
-    this.style.backgroundColor = "red";
-
-
-
-  }
-
-
-})
-
-
-
-
-
 // CLEAR CANVAS BUTTON;
 
 document.getElementById("clearCanvas").onclick = function () {
@@ -76,47 +45,135 @@ colorChange()
 
 // FUNCTION TO DRAW
 
-
-function arcDraw() {
-
-  ctx.arc(mousePos.x, mousePos.y, 5, 0, Math.PI * 2, false)
-
-}
-
-function draw() {
-
-  ctx.beginPath();
-
-
-
-  arcDraw()
-  ctx.fill();
-  ctx.stroke();
-}
-
-
-
 // FUNCTION TO TRACK COORDINATES
-function mouseCoordinates(e) {
+
+
+tools.forEach(t => {
+
+  t.onclick = function () {
+
+    tools.forEach(t => {
+      t.style.backgroundColor = "white"
+    })
+
+    this.style.backgroundColor = "blue";
+
+
+  }
+})
+
+
+
+function findButton() {
+  if (window.getComputedStyle(tools[0], null).getPropertyValue('background-color') === "rgb(0, 0, 255)") {
+    ctx.rect(mousePos.x, mousePos.y, 1, 1)
+
+  } else if (window.getComputedStyle(tools[1], null).getPropertyValue('background-color') === "rgb(0, 0, 255)") {
+    ctx.rect(mousePos.x, mousePos.y, 5, 5)
+
+  } else if (window.getComputedStyle(tools[2], null).getPropertyValue('background-color') === "rgb(0, 0, 255)") {
+    ctx.rect(mousePos.x, mousePos.y, 10, 10)
+
+  } else if (window.getComputedStyle(tools[3], null).getPropertyValue('background-color') === "rgb(0, 0, 255)") {
+
+    ctx.arc(mousePos.x, mousePos.y, 1, 0, Math.PI * 2, false)
+  } else if (window.getComputedStyle(tools[4], null).getPropertyValue('background-color') === "rgb(0, 0, 255)") {
+
+    ctx.arc(mousePos.x, mousePos.y, 3, 0, Math.PI * 2, false)
+  } else if (window.getComputedStyle(tools[5], null).getPropertyValue('background-color') === "rgb(0, 0, 255)") {
+    ctx.arc(mousePos.x, mousePos.y, 6, 0, Math.PI * 2, false)
+  }
+
+}
+
+
+function drawRect(e) {
 
   e = onmousedown || window.event;
   mousePos = {
     x: e.clientX,
     y: e.clientY
   };
-  console.log("hjlk")
-  draw()
+
+  ctx.beginPath();
+  findButton()
+  ctx.fill();
+  ctx.stroke();
+
+
+  // draw()
 }
+
+
+
+canvas.onmousedown = function () {
+  canvas.addEventListener("mousemove", drawRect, false)
+  canvas.onmouseup = function () {
+    canvas.removeEventListener("mousemove", drawRect);
+  }
+}
+
+
+
+// TEST CODE END
+
+// function drawSmallCircle(e) {
+
+//   e = onmousedown || window.event;
+//   mousePos = {
+//     x: e.clientX,
+//     y: e.clientY
+//   };
+
+
+//   ctx.beginPath();
+//   ctx.arc(mousePos.x, mousePos.y, 1, 0, Math.PI * 2, false)
+//   ctx.fill();
+//   ctx.stroke();
+
+
+//   // draw()
+// }
+
+// tools.forEach(t => {
+
+//   t.onclick = function () {
+
+//     switch (this) {
+//       case tools[0]:
+//         canvas.onmousedown = function () {
+//           canvas.addEventListener("mousemove", drawRect, false)
+//           canvas.onmouseup = function () {
+//             canvas.removeEventListener("mousemove", drawRect);
+//           }
+//         }
+//         break;
+//       case tools[1]:
+//         canvas.onmousedown = function () {
+//           canvas.addEventListener("mousemove", drawSmallCircle, false)
+//           canvas.onmouseup = function () {
+//             canvas.removeEventListener("mousemove", drawSmallCircle);
+//           }
+//         }
+//         break;
+//       default:
+//         break;
+//     }
+
+
+//   }
+// })
+
 
 
 
 // MOUSE EVENT HANDLERS
-canvas.onmousedown = function () {
+// canvas.onmousedown = function () {
 
-  canvas.addEventListener("mousemove", mouseCoordinates, false)
+//   canvas.addEventListener("mousemove", mouseCoordinates, false)
 
-  canvas.onmouseup = function () {
-    canvas.removeEventListener("mousemove", mouseCoordinates);
-  }
+//   canvas.onmouseup = function () {
+//     canvas.removeEventListener("mousemove", mouseCoordinates);
+//   }
 
-}
+// }
