fd2746bee03cb404cbfec34958d464219e7ed080
indre101 <zygaityte.indre@gmail.com>
2019-08-27 12:44:47 +0200

    added img

--- excercie/first_ecresize/script.js
+++ excercie/first_ecresize/script.js
@@ -6,8 +6,14 @@ let color = ["blue", "green", "red", "yellow"];
 
 for (let index = 0; index < 20; index++) {
   const myCln = template.cloneNode(true);
+
   myCln.querySelector(".infoText").textContent = "Morning Morning";
-  myCln.querySelector(".infoText").style.backgroundColor = color[Math.floor(Math.random() * color.length)];
+
+  myCln.querySelector(".infoText").style.backgroundColor = color[Math.floor(Math.random() * color.length)]
+
+  myCln.querySelector(".infoText").classList.add("rotate");
+
+  myCln.querySelector(".infoText").classList.add("rotate");
 
   const main = document.querySelector(".main");
 
---
+++ excercie/third/script.js
@@ -0,0 +1,39 @@
+// OBJECT
+
+const dog = {
+  hasFourLegs: true,
+  race: "Canine",
+  breed: "Pug",
+  size: "Small",
+  name: "John",
+  sex: "Female",
+  birthday: {
+    year: 2010,
+    month: 11,
+    day: 1
+  },
+  owners: ["Lasse", "Jonas"],
+  img: "./charles-K4mSJ7kc0As-unsplash (1).jpg"
+}
+
+
+
+const dogTemplate = document.querySelector("template").content;
+const cln = dogTemplate.cloneNode(true);
+const parent = document.querySelector(".main");
+
+
+
+cln.querySelector("h1").textContent = `${dog.name} the ${dog.breed}`;
+cln.querySelector(".race").textContent = `${dog.race}`;
+cln.querySelector(".size").textContent = `${dog.size}`;
+cln.querySelector(".sex").textContent = `${dog.sex}`;
+cln.querySelector(".birthday").textContent = `${dog.birthday.year}-${dog.birthday.month}-${dog.birthday.day}`;
+cln.querySelector(".owners").textContent = `${dog.owners[0]} and ${dog.owners[1]}`;
+
+
+cln.querySelector("img").src = dog.img
+
+
+
+parent.appendChild(cln);
