bba36fe01ca61dd88766ff70d3ac560622e45b50
indre101 <zygaityte.indre@gmail.com>
2019-09-11 11:48:51 +0200

    styled the buttons and the text

---
+++ style-tiles-with-css-master/script.js
@@ -0,0 +1,26 @@
+const btnExpandArr = document.querySelectorAll(".arrowBtn");
+const convertPrototype = Array.prototype.slice.call(btnExpandArr);
+const expandableContentArr = document.querySelectorAll(".expandableContent");
+
+console.log(btnExpandArr)
+
+btnExpandArr.forEach(btn => btn.addEventListener("click", expandContent));
+
+
+function expandContent() {
+
+  btnExpandArr.forEach(btn => btn.classList.remove("rotate"));
+
+  this.classList.toggle("rotate");
+
+  expandableContentArr.forEach(e => {
+    e.classList.add("d-none");
+
+  })
+
+  expandableContentArr[convertPrototype.indexOf(this)].classList.remove("d-none");
+  expandableContentArr[convertPrototype.indexOf(this)].classList.add("expandableContentAnimation");
+
+
+
+}
